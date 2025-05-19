44a0245bcb7afdd0deb993f1341ea8c5829cbfe1
Balbino-teste <balbinojteste@gmail.com>
2025-05-18 21:11:20 -0300

    Criando estrutura do perfil
    Nessa seção foi criado a estrutura do perfil e adicionados os ícones de rede sociais.

---
+++ Profile-React/src/Pages/Profile/index.jsx
@@ -0,0 +1,24 @@
+
+import './style.css'
+
+function Profile() {
+
+  return (
+   <div className="card">
+        
+        <div>
+            <h2>Juliano Balbino</h2>
+            <h3>Desenvolvedor de web sites</h3>
+            <p>Transforming ideas into realities, creating interfaces that inspire and engage users dreams.</p>
+            <p>I am a full-stack developer woth 5 years of experience at Google. i have a passion for modem web Technologies and share my knowledge with others.</p>
+            <div className="socials">
+                <button><i className="fa-brands fa-instagram"></i></button>
+                <button><i className="fa-brands fa-whatsapp"></i></button>
+                <button><i className="fa-brands fa-github"></i></button>        
+            </div>   
+        </div>
+    </div>
+  )
+}
+
+export default Profile
